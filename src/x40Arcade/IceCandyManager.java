1b407f9dda6a7c8406c341335ec4d73415e46ff6
x40arcade <sobrepenakenneth97@gmail.com>
2026-03-26 19:47:32 +0800

    Ver 0.2
    Added
    New Button class
    And basic sign up form not functional yet

--- src/x40Arcade/IceCandyManager.java
+++ src/x40Arcade/IceCandyManager.java
@@ -13,11 +13,14 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class IceCandyManager extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -54,17 +57,19 @@ public class IceCandyManager extends JFrame {
 		JLabel lblTitle = new JLabel("Ice Candy Manager");
 		lblTitle.setForeground(new Color(255, 255, 255));
 		lblTitle.setFont(new Font("Segoe UI Black", Font.BOLD, 30));
-		lblTitle.setBounds(10, 97, 295, 48);
+		lblTitle.setBounds(10, 75, 295, 48);
 		signIn.add(lblTitle);
 
-		JLabel lblDescription = new JLabel("<html> " + "A Java Swing inventory system built to help manage "
-				+ "a small ice candy business." + "</html>");
+		JLabel lblDescription = new JLabel("<html><b> " + "A Java Swing inventory system built to help manage "
+				+ "a small ice candy business." + "</b></html>");
 		lblDescription.setForeground(Color.WHITE);
 		lblDescription.setFont(new Font("Segoe UI", Font.PLAIN, 12));
 		lblDescription.setBounds(10, 144, 295, 37);
 		signIn.add(lblDescription);
 
 		KenGradientButton btnSignIn = new KenGradientButton("Sign In");
+		btnSignIn.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		btnSignIn.setForeground(new Color(255, 255, 255));
 		btnSignIn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Clicked!");
@@ -82,8 +87,37 @@ public class IceCandyManager extends JFrame {
 
 		JLabel lblNewLabel = new KenGradientLabel("Create Account");
 		lblNewLabel.setFont(new Font("Segoe UI Black", Font.BOLD, 30));
-		lblNewLabel.setBounds(41, 59, 272, 41);
+		lblNewLabel.setBounds(25, 77, 272, 41);
 		signUp.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Name");
+		lblNewLabel_1.setForeground(new Color(255, 94, 58));
+		lblNewLabel_1.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(54, 127, 77, 27);
+		signUp.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(134, 133, 128, 20);
+		signUp.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Password");
+		lblNewLabel_1_1.setForeground(new Color(255, 94, 58));
+		lblNewLabel_1_1.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(54, 158, 77, 27);
+		signUp.add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(134, 164, 128, 20);
+		signUp.add(textField_1);
+		
+		KenGradientButton kngrdntbtnSignUp = new KenGradientButton("Sign In");
+		kngrdntbtnSignUp.setText("Sign Up");
+		kngrdntbtnSignUp.setForeground(Color.WHITE);
+		kngrdntbtnSignUp.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		kngrdntbtnSignUp.setBounds(115, 196, 89, 23);
+		signUp.add(kngrdntbtnSignUp);
 
 	}
 }
--- src/x40Arcade/KenGradientButton.java
+++ src/x40Arcade/KenGradientButton.java
@@ -8,58 +8,54 @@ import java.awt.*;
  */
 public class KenGradientButton extends JButton {
 
+	private boolean pressed = false;
 
-    private boolean pressed = false;
+	private Color color1 = new Color(255, 94, 58);
+	private Color color2 = new Color(255, 195, 113);
 
-    private Color color1 = new Color(255, 140, 0);
-    private Color color2 = new Color(255, 220, 130);
+	public KenGradientButton(String text) {
+		super(text);
 
-    public KenGradientButton(String text) {
-        super(text);
+		setContentAreaFilled(false);
+		setFocusPainted(false);
+		setBorderPainted(false);
 
-        setContentAreaFilled(false);
-        setFocusPainted(false);
-        setBorderPainted(false);
+		addMouseListener(new java.awt.event.MouseAdapter() {
 
-        addMouseListener(new java.awt.event.MouseAdapter() {
+			@Override
+			public void mousePressed(java.awt.event.MouseEvent e) {
+				pressed = true;
+				repaint();
+			}
 
-            @Override
-            public void mousePressed(java.awt.event.MouseEvent e) {
-                pressed = true;
-                repaint();
-            }
+			@Override
+			public void mouseReleased(java.awt.event.MouseEvent e) {
+				pressed = false;
+				repaint();
+			}
+		});
+	}
 
-            @Override
-            public void mouseReleased(java.awt.event.MouseEvent e) {
-                pressed = false;
-                repaint();
-            }
-        });
-    }
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
+		Color top;
+		Color bottom;
 
-        Color top;
-        Color bottom;
+		if (pressed) {
+			top = new Color(200, 110, 0);
+			bottom = new Color(240, 180, 100);
+		} else {
+			top = color1;
+			bottom = color2;
+		}
 
-        if (pressed) {
-            top = new Color(200, 110, 0);
-            bottom = new Color(240, 180, 100);
-        } else {
-            top = color1;
-            bottom = color2;
-        }
+		GradientPaint gp = new GradientPaint(0, 0, top, 0, getHeight(), bottom);
 
-        GradientPaint gp = new GradientPaint(
-            0, 0, top,
-            0, getHeight(), bottom
-        );
+		g2d.setPaint(gp);
+		g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
 
-        g2d.setPaint(gp);
-        g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
-
-        super.paintComponent(g);
-    }
+		super.paintComponent(g);
+	}
 }
